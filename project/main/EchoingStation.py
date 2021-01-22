fdd5b0d3d9bb002308c8612e09c2921302fd2b06
Jacob <jacob.johnsson1992@gmail.com>
2021-01-22 19:34:19 +0100

    compiler...

--- project/main/EchoingStation.py
+++ project/main/EchoingStation.py
@@ -1,14 +1,15 @@
 import digitalio, board, busio, adafruit_rfm9x
 
-class Station:
+RADIO_FREQ_MHZ = 868.
 
-    RADIO_FREQ_MHZ = 868.
+CS = digitalio.DigitalInOut( board.CE1)
+RESET = digitalio.DigitalInOut( board.D25)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
-    CS = digitalio.DigitalInOut( board.CE1)
-    RESET = digitalio.DigitalInOut( board.D25)
-    spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+radio = adafruit_rfm9x.RFM9x( spi, CS, RESET, RADIO_FREQ_MHZ)
 
-    radio = adafruit_rfm9x.RFM9x( spi, CS, RESET, RADIO_FREQ_MHZ)
+class EchoingStation:
+
 
     def __init__(self):
         pass
@@ -20,7 +21,7 @@ class Station:
             radio.send(packet)
 
 def main():
-    station = Station()
+    station = EchoingStation()
     while True:
         station.do()
 
