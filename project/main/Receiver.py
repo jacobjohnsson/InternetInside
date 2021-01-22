ce4bde14c619792ca25a81eb424db4c890f1bf89
Jacob <jacob.johnsson1992@gmail.com>
2021-01-22 19:10:41 +0100

    Working Sender and Receiver

--- project/main/Receiver.py
+++ project/main/Receiver.py
@@ -1,12 +1,23 @@
-import PyLora
+import digitalio, board, busio, adafruit_rfm9x
 import time
-PyLora.init()
-PyLora.set_frequency(915000000)
-PyLora.enable_crc()
-while True:
-    PyLora.receive()   # put into receive mode
-    while not PyLora.packet_available():
-        # wait for a package
-        time.sleep(0)
-    rec = PyLora.receive_packet()
-    print('Packet received: {}'.format(rec))
+
+RADIO_FREQ_MHZ = 868.
+
+CS = digitalio.DigitalInOut( board.CE1)
+RESET = digitalio.DigitalInOut( board.D25)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+radio = adafruit_rfm9x.RFM9x( spi, CS, RESET, RADIO_FREQ_MHZ)
+
+counter = 0;
+t0 = time.perf_counter()
+
+while(time.perf_counter() - t0 < 600):
+	packet = radio.receive()
+	rssi = radio.last_rssi
+	#print("Signal strength: " + str(rssi) + " dB")
+	if (packet != None):
+		counter += 1
+	#print(str(counter) + "\t[" + str(packet) + "]")
+
+print("Received " + str(counter) + " packages")
--- project/main/Sender.py
+++ project/main/Sender.py
@@ -1,9 +1,24 @@
-import PyLora
+import array
+import digitalio, board, busio, adafruit_rfm9x
+import numpy as np
 import time
-PyLora.init()
-PyLora.set_frequency(915000000)
-PyLora.enable_crc()
-while True:
-    PyLora.send_packet('Hello')
-    print('Packet sent...')
-    time.sleep(2)
+
+RADIO_FREQ_MHZ = 868.
+
+CS = digitalio.DigitalInOut(board.CE1)
+RESET = digitalio.DigitalInOut(board.D25)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+radio = adafruit_rfm9x.RFM9x( spi, CS, RESET, RADIO_FREQ_MHZ)
+radio.tx_power = 5
+b = bytearray()
+b.extend(map(ord, "Hello World"))
+
+counter = 0
+t0 = time.perf_counter()
+
+while (time.perf_counter() - t0 < 595):
+	result = radio.send(b)
+	counter += 1;
+
+print(counter)
