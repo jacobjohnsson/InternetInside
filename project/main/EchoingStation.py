f4996bc4c195e9038b0ac0201cff671e807894b2
Jacob <jacob.johnsson1992@gmail.com>
2021-01-27 13:00:49 +0100

    Nicer Echo with addressing

--- project/main/EchoingStation.py
+++ project/main/EchoingStation.py
@@ -1,4 +1,5 @@
 import digitalio, board, busio, adafruit_rfm9x
+import time
 
 RADIO_FREQ_MHZ = 868.
 
@@ -7,18 +8,25 @@ RESET = digitalio.DigitalInOut( board.D25)
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
 radio = adafruit_rfm9x.RFM9x( spi, CS, RESET, RADIO_FREQ_MHZ)
+radio.destination = 7
+radio.node = 8
 
 class EchoingStation:
-
 
     def __init__(self):
         pass
 
     def do(self):
-        packet = radio.receive()
+        packet = radio.receive(with_header=True, timeout = 5.0)
         if packet != None:
-            print("Echoing " + str(packet))
-            radio.send(packet)
+            data = packet[4:]
+            src = packet[1]
+            dest = packet[0]
+            print("Echoing: \t" + str(data) + 
+            "\n\t\tFrom:\t" + str(src) + 
+            "\n\t\tTo:\t" + str(dest))
+            #time.sleep(0.2)
+            radio.send(data)
 
 def main():
     station = EchoingStation()
