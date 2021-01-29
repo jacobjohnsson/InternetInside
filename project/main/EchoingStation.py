157b58e1e09befffb3e5db778b4219cdccc2b64b
Jacob <jacob.johnsson1992@gmail.com>
2021-01-29 14:00:22 +0100

    Addressing changes to EchoStation

--- project/main/EchoingStation.py
+++ project/main/EchoingStation.py
@@ -1,15 +1,23 @@
 import digitalio, board, busio, adafruit_rfm9x
 import time
 
-RADIO_FREQ_MHZ = 868.
+RADIO_FREQ_MHZ_1 = 866.
+RADIO_FREQ_MHZ_2 = 867.
 
 CS = digitalio.DigitalInOut( board.CE1)
 RESET = digitalio.DigitalInOut( board.D25)
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
-radio = adafruit_rfm9x.RFM9x( spi, CS, RESET, RADIO_FREQ_MHZ)
-radio.destination = 7
-radio.node = 8
+tranceiver = adafruit_rfm9x.RFM9x( spi, CS, RESET, RADIO_FREQ_MHZ_2)
+tranceiver.destination = 7
+tranceiver.node = 8
+
+CS = digitalio.DigitalInOut(board.D17)
+RESET = digitalio.DigitalInOut(board.D4)
+spi = busio.SPI(board.D21, MOSI=board.D20, MISO=board.D19)
+receiver = adafruit_rfm9x.RFM9x( spi, CS, RESET, RADIO_FREQ_MHZ_1)
+receiver.tx_power = 5
+receiver.node = 8
 
 class EchoingStation:
 
@@ -17,7 +25,7 @@ class EchoingStation:
         pass
 
     def do(self):
-        packet = radio.receive(with_header=True, timeout = 5.0)
+        packet = receiver.receive(with_header=True, timeout = 5.0)
         if packet != None:
             data = packet[4:]
             src = packet[1]
@@ -26,7 +34,7 @@ class EchoingStation:
             "\n\t\tFrom:\t" + str(src) + 
             "\n\t\tTo:\t" + str(dest))
             #time.sleep(0.2)
-            radio.send(data)
+            tranceiver.send(data)
 
 def main():
     station = EchoingStation()
