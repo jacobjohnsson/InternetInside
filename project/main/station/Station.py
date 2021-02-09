9439cb0d2d434f3f4845d3e78f00412421de27bf
Jacob <jacob.johnsson1992@gmail.com>
2021-02-09 10:26:25 +0100

    Add Tunstation

--- project/main/station/Station.py
+++ project/main/station/Station.py
@@ -1,28 +1,69 @@
 import board
 import time
 import threading
+import pytun
 
 from multiprocessing import Queue, Process
 
 terminate_station = False
 
-class Station:
+class TunStation:
 
-    def __init__(self, receiver: board, transmitter: board):
-        self.strategy = NoStrategy()
+    tx_queue = Queue(200)
+    rx_queue = Queue(400)
+
+    def __init__(self, receiver: board, transmitter: board, address: int, tun):
+        self.tun = tun
+        self.address = address
         self.receiver = receiver
+        self.receiver.node = address
+        self.rx_process = Process(target=self.blocking_receive, kwargs={'queue':self.rx_queue})
+        self.rx_process.start()
+
         self.transmitter = transmitter
+        self.transmitter.node = address
+        self.tx_process = Process(target=self.blocking_send, kwargs={'queue':self.tx_queue})
+        self.tx_process.start()
+        
 
-    def setStrategy(self, strategy):
-        self.strategy = strategy
-        self.strategy.setReceiver(self.receiver)
-        self.strategy.setTransmitter(self.transmitter)
+    def shutdown(self):
+        self.receiver.reset()
+        self.transmitter.reset()
+        self.rx_process.terminate()
+        self.tx_process.terminate()
 
-    def send(self, message):
-        self.strategy.send(message)
+    def send(self, message, dest):
+        self.tx_queue.put((message, dest))
+        #print("SendQueue size: " + tx_queue.qsize())
+
+    def blocking_send(self, queue):
+        while True:
+            message = self.tx_queue.get()
+            data = message[0]
+            dest = message[1]
+            self.transmitter.send(data, destination=dest)
 
     def receive(self) -> str:
-        return self.strategy.receive()
+        #print("Receive-Size: " + str(self.rx_queue.qsize()))
+        return self.rx_queue.get()
+
+    def receive_timeout(self, timeout) -> str:
+        #print("Receive-Size: " + str(self.rx_queue.qsize()))
+        try:
+            return self.rx_queue.get(timeout=timeout)
+        except:
+            return None
+
+    def blocking_receive(self, queue):
+        while True:
+            message = self.receiver.receive(with_header=True)
+            if message != None:
+                self.rx_queue.put(message)
+                self.tun.write(bytes(message[4:]))
+                print("Printing " + str(message) + " to tun.\n")
+
+    def tx_queue_size(self):
+        return self.tx_queue.qsize()
 
 class ThreadedStation:
 
@@ -75,40 +116,5 @@ class ThreadedStation:
             if message != None:
                 self.rx_queue.put(message)
 
-
-class NoStrategy:
-
-    def __init__(self):
-        pass
-
-    def send(self, message):
-        pass
-
-    def receive(self):
-        pass
-
-    def setReceiver(self, receiver: board):
-        pass
-
-    def setTransmitter(self, transmitter: board):
-        pass
-
-class BasicStrategy(NoStrategy):
-
-    def __init__(self, receiver: board, transmitter: board):
-        self.receiver = receiver
-        self.transmitter = transmitter
-
-    def send(self, message):
-        b = bytearray()
-        b.extend(map(ord, message))
-        self.transmitter.send(b)
-
-    def receive(self) -> bytearray:
-        return self.receiver.receive()
-
-    def setReceiver(self, receiver: board):
-        self.receiver = receiver
-
-    def setTransmitter(self, transmitter: board):
-        self.transmitter = transmitter
+    def tx_queue_size(self):
+        return self.tx_queue.qsize()
